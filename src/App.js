522477a1014c23e82fdd0ec787a1ac4dcb96a3f3
arisharma43 <arisharma2017@gmail.com>
2022-07-16 17:26:25 -0500

    CRUD Functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, FormControl, Input, InputLabel } from "@mui/material";
 import Todo from "./Todo";
+import db from "./firebase";
+import firebase from "firebase/compat/app";
+import { query, orderBy, limit } from "firebase/firestore";
 
 function App() {
 	const [todos, setTodos] = useState([]);
 	const [input, setInput] = useState("");
 
-	//useEffect(() => {}, dependencies);
+	useEffect(() => {
+		db.collection("todos")
+			.orderBy("timestamp", "desc")
+			.onSnapshot((snapshot) => {
+				setTodos(
+					snapshot.docs.map((doc) => ({ id: doc.id, todo: doc.data().todo }))
+				);
+			});
+	}, []);
 
 	const addTodo = (event) => {
 		event.preventDefault(); // prevents refresh event
-		console.log("It's working!");
+
+		db.collection("todos").add({
+			todo: input,
+			timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+		});
+
 		setTodos([...todos, input]);
 		setInput(""); //clear up the input after submitting the button
 	};
@@ -41,7 +57,7 @@ function App() {
 
 			<ul>
 				{todos.map((todo) => (
-					<Todo text={todo} />
+					<Todo todo={todo} />
 					// <li>{todo}</li>
 				))}
 			</ul>
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,15 +1,82 @@
-import { List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
-import React from "react";
+import {
+	Button,
+	List,
+	ListItem,
+	ListItemAvatar,
+	ListItemText,
+	Modal,
+} from "@mui/material";
+// import { makeStyles } from "@mui/material/styles";
+import React, { useState } from "react";
+import {
+	ThemeProvider,
+	createMuiTheme,
+	makeStyles,
+} from "@mui/material/styles";
 import "./Todo.css";
+import db from "./firebase";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+
+// const useStyles = makeStyles((theme) => ({
+// 	paper: {
+// 		position: "absolute",
+// 		width: 400,
+// 		backgroundColor: theme.palette.background.paper,
+// 		border: "2px solid #000",
+// 		boxShadow: theme.shadows[5],
+// 		padding: theme.spacing(2, 4, 3),
+// 	},
+// }));
 
 function Todo(props) {
+	//const classes = useStyles();
+
+	const [open, setOpen] = useState(false);
+	const [input, setInput] = useState();
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	const updateTodo = () => {
+		//update the todo with new input text
+		db.collection("todos").doc(props.todo.id).set(
+			{
+				todo: input,
+			},
+			{ merge: true }
+		);
+		setOpen(false);
+	};
+
 	return (
-		<List className="todo_list">
-			<ListItem>
-				<ListItemAvatar></ListItemAvatar>
-				<ListItemText primary={props.text} />
-			</ListItem>
-		</List>
+		<>
+			<Modal open={open} onClose={(e) => setOpen(false)}>
+				<div>
+					<h1> I am a Modal! </h1>
+					<input
+						placeholder={props.todo.todo}
+						value={input}
+						onChange={(event) => setInput(event.target.value)}
+					></input>
+					<Button onClick={updateTodo}>Update Todo</Button>
+				</div>
+			</Modal>
+			<List className="todo_list">
+				<ListItem>
+					<ListItemAvatar></ListItemAvatar>
+					<ListItemText primary={props.todo.todo} />
+				</ListItem>
+				<Button onClick={(e) => setOpen(true)}> Edit</Button>
+				<DeleteForeverIcon
+					onClick={(event) =>
+						db.collection("todos").doc(props.todo.id).delete()
+					}
+				>
+					Delete
+				</DeleteForeverIcon>
+			</List>
+		</>
 	);
 }
 
